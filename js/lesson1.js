83053bfe873d304484f6951f895378da791bd225
andriy <bartush33@gmail.com>
2024-05-04 16:11:28 +0300

    Update lesson1.js

--- js/lesson1.js
+++ js/lesson1.js
@@ -33,3 +33,98 @@
 //числа от max до min по зменьшенню
 //Виведіть в консоль суму усіх парних чисел
 
+
+// const max = 100;
+// const min = 10;
+// let total = 0;
+// for (let i = max; i >= min; i--) {
+//     console.log(i); 
+//     if (i % 2 === 0) {
+//         total += i;
+//     }    
+// }
+// console.log(total);
+
+// function numbers (max, min){
+//     let total = 0;
+//     for (let i = max; i >= min; i--) {
+//         console.log(i); 
+//         if (i % 2 === 0) {
+//             total += i;
+//         }    
+//     }
+//     return total;
+    
+// }
+// console.log(numbers (100, 10));
+
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+
+// const num = Number(prompt("введіть цифру від 1 до 4"));
+// let result = "";
+// switch (num) {
+//     case 1:
+//         result = 'зима';
+//         break;
+//     case 2:
+//         result = 'весна';
+//         break;
+//         case 3:
+//         result = 'літо';
+//         break;
+//         case 4:
+//         result = 'осінь';
+//         break;
+//     default:
+//         result = "введіть цифру від 1 до 4";
+// }
+//     console.log(result);
+
+// const num = Number(prompt("введіть цифру від 1 до 4"));
+// function season(num) {
+//     switch (num) {
+//             case 1:
+//                 return 'зима';
+//             case 2:
+//                 return 'весна';
+//             case 3:
+//                 return 'літо';
+//             case 4:
+//                 return 'осінь';
+//             default:
+//                 return "введіть цифру від 1 до 4";
+//         }
+//     }
+//     console.log(season(num));
+
+//Напишіть код, який буде питати
+//логін за допомогою prompt и логіровати результат
+//в консоль браузера
+//Якщо користувач вводить "Адмін",
+//то prompt запрашує пароль.
+//Якщо ничого не ввели чи нажата клавіша Esc
+//вивести строку "Скасовано"
+//В противному випадку вивести рядок "Я вас не знаю"
+//Пароль перевіряти так:
+//Якщо введен пароль "Я головний",
+//то вивести рядок "Вітаю!"
+//в іншому випадку виводити рядок "Невірний пароль!"
+
+// const login = prompt("введіть логін");
+// console.log(login);
+// if (login === "Адмін") {
+//     const password = prompt("Введіть пароль");
+//     // if (password === "Я головний") {
+//     //     alert("Вітаю!");
+//     // } else {
+//     //     alert("Невірний пароль!");
+//     // }
+//     alert(password === "Я головний" ? "Вітаю!" : "Невірний пароль!");
+// }
+// else if (login === "" || login === null) {
+//     alert("Скасовано");
+// } else {
+//     alert("Я вас не знаю");
+// }
