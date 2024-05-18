a341699b6edacb3a620d90189ba90423b01a1e38
andriy <bartush33@gmail.com>
2024-05-18 15:17:06 +0300

    Update lesson2.js

--- js/lesson2.js
+++ js/lesson2.js
@@ -7,21 +7,41 @@
 //<номер елемента> - <значення елемента>
 //Нумерація має починатись з 1
 
-const styles = ['Джаз', 'Блюз'];
-styles.push('Рок-н-ролл');
-// styles[1] = 'Класика';
-const index = styles.indexOf('Блюз');
-if (index !== -1) {
-  styles[index] = 'Класика';
+// const styles = ['Джаз', 'Блюз'];
+// styles.push('Рок-н-ролл');
+// // styles[1] = 'Класика';
+// const index = styles.indexOf('Блюз');
+// if (index !== -1) {
+//   styles[index] = 'Класика';
 
-}
+// }
 
-function logItems (array) {
-  for (let i = 0; i < array.length; i +=1) {
-    console.log (`${i + 1} - ${array[i]}`);
+// function logItems (array) {
+//   for (let i = 0; i < array.length; i +=1) {
+//     console.log (`${i + 1} - ${array[i]}`);
     
-  }
+//   }
+
+// }
+
+// logItems(styles);
+
+// Напиши функцію, яка перебирає масив логінів і перевіряє
+// чи є ім'я введене в інпут у цьому масиві і у разі,
+// якщо є - виводить повідомлення "Доступ дозволено"
+// в іншому випадку - "Користувач не знайден"
+const logins = ["Peter", "John", "Igor", "Sasha"];
+
+function checkLogin (logins) {
+  const login = prompt("Enter Login");
+  // for (const name of logins) {
+  //   if (name === login) {
+  //     return "Доступ дозволено";
+  //   } 
+  // }
+  // return "Користувач не знайден";
+  return logins.includes(login) ? "Доступ дозволено" : "Користувач не знайден";
+
 
 }
-
-logItems(styles);
+console.log(checkLogin(logins));
